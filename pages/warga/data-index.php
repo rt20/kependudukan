5d0e83d623827ba1a781e3bc1377193a7513db13
rt20 <kaploks@gmail.com>
2022-07-20 22:50:26 +0700

    first commit

---
+++ pages/warga/data-index.php
@@ -0,0 +1,28 @@
+<?php
+include('../../config/koneksi.php');
+
+if ($_SESSION['user']['status_user'] != 'Admin'){
+  $iduser = $_SESSION['user']['id_user'];
+  // ambil dari database
+  $query = "SELECT *, TIMESTAMPDIFF(YEAR, `tanggal_lahir_warga`, CURDATE()) AS usia_warga FROM warga where id_user=$iduser";
+  
+  $hasil = mysqli_query($db, $query);
+  
+  $data_warga = array();
+  
+  while ($row = mysqli_fetch_assoc($hasil)) {
+    $data_warga[] = $row;
+  }
+} else {
+   // ambil dari database
+   $query = "SELECT *, TIMESTAMPDIFF(YEAR, `tanggal_lahir_warga`, CURDATE()) AS usia_warga FROM warga";
+  
+   $hasil = mysqli_query($db, $query);
+   
+   $data_warga = array();
+   
+   while ($row = mysqli_fetch_assoc($hasil)) {
+     $data_warga[] = $row;
+   }
+}
+
